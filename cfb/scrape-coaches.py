d4884e834cd6e0ca1b165f2644ac0f1be1e42fec
ryanvmenezes <ryanvmenezes@gmail.com>
2017-10-10 21:30:03 -0700

    new stab

---
+++ cfb/scrape-coaches.py
@@ -0,0 +1,36 @@
+import csv
+import time
+import requests
+from tqdm import tqdm
+from bs4 import BeautifulSoup
+
+URL = 'https://www.sports-reference.com/cfb/schools/{}/'
+
+teams = []
+with open('sportsref-teams.psv') as csvfile:
+    reader = csv.DictReader(csvfile, delimiter='|')
+    for row in reader:
+        if row['category'] == 'major':
+            teams.append(row)
+
+results = []
+for tm in tqdm(teams):
+    r = requests.get(URL.format(tm['slug']))
+    soup = BeautifulSoup(r.text, "html5lib")
+
+    tbl = soup.find("table", "stats_table").tbody.find_all('tr')
+    for t in tbl:
+        if not t.get('class'):
+            res = {}
+            res['team'] = tm['slug']
+            res['year'] = t.find(attrs={"data-stat": "year_id"}).text
+            res['coaches_text'] = t.find(attrs={"data-stat": "coaches"}).text
+            results.append(res)
+
+    time.sleep(0.5)
+
+headers = ['team','year','coaches_text']
+with open('sportsref-coaches.csv', 'w') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=headers)
+    writer.writeheader()
+    writer.writerows(results)
