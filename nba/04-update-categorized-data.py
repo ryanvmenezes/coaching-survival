3c77cebe5949c71602cf3bb8311711c9fe932d85
ryanvmenezes <ryanvmenezes@gmail.com>
2017-12-15 11:50:17 -0800

    freeze

--- nba/04-update-categorized-data.py
+++ nba/04-update-categorized-data.py
@@ -1,25 +1,50 @@
 import settings
 
-rawpoc = settings.read_csv('data/coach-list.csv')
+## update POC categorization
+
+rawcoaches = settings.read_csv('data/coach-list.csv')
 
 oldpoc =  {
     l['coach_id']: (l['poc'], l['note'])
     for l in settings.read_csv('categorized/coach-list-poc.csv')
 }
 
-for coach in rawpoc:
+for coach in rawcoaches:
     poc = note = ''
     if coach['coach_id'] in oldpoc:
         poc, note = oldpoc[coach['coach_id']]
     coach['poc'] = poc
     coach['note'] = note
 
-rawpoc = sorted(rawpoc, key=lambda x: (x['poc'], x['coach']), reverse=True)
+rawcoaches = sorted(rawcoaches, key=lambda x: (x['poc'], x['coach']), reverse=True)
 
 settings.write_csv(
-    rawpoc,
+    rawcoaches,
     'categorized/coach-list-poc.csv',
     ['coach','coach_id','franchises','poc','note'],
+)
+
+## update NBA player categorization
+
+rawcoaches = settings.read_csv('data/coach-list.csv')
+
+oldplayed =  {
+    l['coach_id']: l['former_nba_player']
+    for l in settings.read_csv('categorized/coach-list-qualifications.csv')
+}
+
+for coach in rawcoaches:
+    former_player = ''
+    if coach['coach_id'] in oldplayed:
+        former_player = oldplayed[coach['coach_id']]
+    coach['former_nba_player'] = former_player
+
+rawcoaches = sorted(rawcoaches, key=lambda x: x['former_nba_player'], reverse=True)
+
+settings.write_csv(
+    rawcoaches,
+    'categorized/coach-list-poc.csv',
+    ['coach','coach_id','franchises','former_nba_player'],
 )
 
 
