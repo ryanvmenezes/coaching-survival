18d7d61b53bd24c0affa15f9fc0eec12517e8faa
Ryan Menezes <ryanvmenezes@gmail.com>
2020-06-16 01:15:23 -0700

    study set-up

--- nba/01_create-study.R
+++ nba/01_create-study.R
@@ -1,6 +1,7 @@
 library(glue)
 library(rvest)
 library(tidyverse)
+library(googlesheets4)
 
 source('nba/utils.R')
 
@@ -20,11 +21,17 @@ coach.names = read_csv(glue('{outfolder}scrape/processed/coaches.csv'))
 
 coach.names
 
-# subset tenures
-# looking at coaching tenures from the 1998-99 season to 2018-19
+# subset tenures to just ones that were active in 2000, and all after it
 
-tenures.study = tenures %>% 
-  filter(start.year >= 1999 | (start.year <= 2000 & end.year >= 2000))
+tenures.study = coach.names %>% 
+  select(coach.id, name) %>% 
+  right_join(
+    tenures %>% 
+    filter(start.year >= 2000 | (start.year <= 2000 & end.year >= 2000))
+  ) %>% 
+  arrange(franchise.id, tenure.id)
+
+tenures.study
 
 # who are the coaches in this period?
 
@@ -39,10 +46,37 @@ coaches.study = coach.names %>%
   
 coaches.study
 
+# join to manually categorized data ---------------------------------------
+
+sheeturl = 'https://docs.google.com/spreadsheets/d/1CnFI-QxRaKwt_MUUup68a8makUGL2jpwXium_H0l4Tc/edit#gid=0'
+gs4_auth(email = 'ryanvmenezes@gmail.com')
+
 # coaches need to be categorized as POC, qualifications, etc
 
-coaches.study %>% write_csv(glue('{outfolder}processed/coaches.csv'), na = '')
+coaches.tagged = read_sheet(sheeturl, sheet = 'nba-coaches')
+
+coaches.tagged
+
+coaches.joined = coaches.study %>% 
+  left_join(
+    coaches.tagged %>%
+      select(-franchises)
+  )
+
+coaches.joined
+
+coaches.joined %>% write_csv(glue('{outfolder}processed/coaches.csv'), na = '')
+coaches.joined %>% write_sheet(ss = sheeturl, sheet = 'nba-coaches')
 
 # tenures need to be marked by how they ended
 
-tenures.study %>% write_csv(glue('{outfolder}processed/tenures.csv'), na = '')
+tenures.tagged = read_sheet(sheeturl, sheet = 'nba-tenures')
+
+tenures.joined = tenures.study %>% 
+  left_join(
+    tenures.tagged %>% 
+      select(tenure.slug, tenure.ending, interim.promoted)
+  )
+
+tenures.joined %>% write_csv(glue('{outfolder}processed/tenures.csv'), na = '')
+tenures.joined %>% write_sheet(ss = sheeturl, sheet = 'nba-tenures')
